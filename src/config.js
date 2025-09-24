f3b576489290401c44f9111514c763606349e969
ahmadalammar <ahmadalammar@airasia.com>
2025-09-24 08:04:06 +0800

    fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 const config = {
-  API_BASE_URL: 'http://localhost:3001',
-  CARD_BASE_URL: 'http://localhost:5173',
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001',
+  CARD_BASE_URL: import.meta.env.VITE_CARD_BASE_URL || 'http://localhost:5173',
 }
+
+console.log('API_BASE_URL:', config.API_BASE_URL)
 
 export default config
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import autoprefixer from 'autoprefixer'
-import config from './src/config'
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
   return {
     base: './',
     build: {
@@ -44,7 +44,7 @@ export default defineConfig(() => {
       port: 3005,
       proxy: {
         '/uploads': {
-          target: config.API_BASE_URL,
+          target: env.VITE_API_BASE_URL || 'http://localhost:3001',
           changeOrigin: true,
         },
       },
