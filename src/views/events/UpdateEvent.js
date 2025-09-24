69519315175d0bd96beb1631e7fe28ee43cdf53b
ahmadalammar <ahmadalammar@airasia.com>
2025-09-24 19:54:25 +0800

    fix

--- src/views/events/UpdateEvent.js
+++ src/views/events/UpdateEvent.js
@@ -63,15 +63,10 @@ const UpdateEvent = () => {
         setSchedules(
           (data.events || []).map((event) => {
             if (!event.date) return { ...event, date: '' }
-            const localDate = new Date(event.date)
-            const year = localDate.getFullYear()
-            const month = String(localDate.getMonth() + 1).padStart(2, '0')
-            const day = String(localDate.getDate()).padStart(2, '0')
-            const hours = String(localDate.getHours()).padStart(2, '0')
-            const minutes = String(localDate.getMinutes()).padStart(2, '0')
+            const date = event.date.slice(0, 16)
             return {
               ...event,
-              date: `${year}-${month}-${day}T${hours}:${minutes}`,
+              date: date,
             }
           }),
         )
